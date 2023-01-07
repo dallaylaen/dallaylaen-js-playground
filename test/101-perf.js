13767e696684ae137e1af8f101cf1103edc9dde3
Konstantin S. Uvarin <khedin@gmail.com>
2023-01-07 15:03:03 +0100

    [lib] Add benchmark draft code

---
+++ test/101-perf.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const { Perf } = require('../lib/perf.js');
+
+describe('Perf', () => {
+  it('runs some code & provides summary', (done) => {
+    const perf = new Perf()
+      .setup((n) => [...Array(n).keys()].reverse())
+      .teardown((array) => {
+        for (let i = 1; i < array.length; i++) if (array[i - 1] > array[i]) throw new Error('foo bared');
+      });
+
+    const out = perf.run((ary) => ary.sort((x, y) => x - y), 10000);
+
+    expect(out.user).to.be.within(0, Infinity);
+    expect(out.system).to.be.within(0, Infinity);
+    expect(out.elapsed).to.be.within(0, Infinity);
+
+    console.log(out);
+
+    done();
+  });
+});
