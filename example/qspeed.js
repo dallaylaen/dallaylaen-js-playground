6e0636a1f469ce4f96c8c47d3cdc83c9ff149231
Konstantin S. Uvarin <khedin@gmail.com>
2023-01-20 01:04:47 +0100

    [bench] Play around with example a bit more

--- example/qspeed.js
+++ example/qspeed.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const { BigoBench } = require( '../lib/bigobench' );
+const refute = require( 'refutable' );
 const { PQueue } = require( '../queue/pqueue-closure' );
+
+
+const FETCH = 100;
 
 const bench = new BigoBench()
   .setup( (n, cb) => {
     cb( [...new Array(n).keys()].sort( () => Math.random() - 0.5 ) );
+  })
+  .teardown( (ret, cb ) => {
+    const report = refute.report( r => {
+      r.type(ret, 'array');
+      r.equal( ret.length, FETCH );
+      r.ordered( 'ret is ascending', ret, (r, x, y) => {
+        r.cmpNum(x, '<', y);
+      })
+    });
+    cb( report.getPass() ? undefined : ''+report );
   });
 
 const probe = (list, cb) => {
@@ -13,10 +27,12 @@ const probe = (list, cb) => {
   for (const i of list)
     pq.push(i);
   const out = [];
-  while (pq.length())
+  for (let i = 0; i < FETCH; i++)
     out.push( pq.shift() );
   cb(out);
 }
 
-for (const n of [10 ** 4, 10 ** 5 , 10 ** 6])
-  bench.run( n, probe ).then(console.log);
+bench.compare({minArg: 100, maxArg: 1000000, maxTime: 1}, {
+  pqueue: probe,
+  sort: (list, cb) => cb(list.sort((x, y) => x - y).slice(0, FETCH)),
+}).then(console.log);
