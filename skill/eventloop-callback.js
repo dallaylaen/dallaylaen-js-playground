2a8614530dc3698aa772adf48d2fa4a3fe40df65
Konstantin S. Uvarin <khedin@gmail.com>
2023-09-17 01:57:53 +0200

    [skill] Test how different 'immediate' execution patterns behave

---
+++ skill/eventloop-callback.js
@@ -0,0 +1,27 @@
+/**
+ *  Test how iterating over the same function behaves dependent on whether
+ *  it's a direct callback, promise, setImmediate, setTimeout(0), or process.nextTick
+ *
+ */
+
+const iter = 100;
+
+
+setImmediate(() => {
+  redo('immediate', iter, 0, setImmediate);
+  redo('timeout', iter, 0, f => setTimeout(f, 0));
+  redo('nextTick', iter, 0, process.nextTick);
+  redo('promise', iter, 0, f => new Promise(f));
+  redo('plain js', iter, 0, f => f());
+});
+
+
+
+function redo(name, max, n, cb) {
+  if (n >= max) {
+    console.log(name, 'finished');
+    return;
+  }
+  console.log(name, n);
+  cb(() => redo(name, max, n+1, cb));
+}
