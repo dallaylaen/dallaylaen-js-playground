e00d059df783b8bccfeb07f7041f396e15440069
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-16 01:51:33 +0200

    [ski] combinators now work

--- skill/ski.js
+++ skill/ski.js
@@ -4,30 +4,74 @@
 
 class Ast {
   constructor() {};
-   apply(x) {
-    return new Call(this, x);
+  combine(args) {
+    if (args instanceof Ast)
+      args = [args];
+    return args.length > 0 ? new Call(this, args) : this;
   }
-   eval() {return null};
-   toString() {
+  eval() {return null};
+  toString() {
     throw new Error( "toString() undefined for generic AST" );
   }
 }
 
 class Call extends Ast {
-  constructor (fun, arg) {
+  /**
+   *
+   * @param {Ast} fun
+   * @param {Ast[]} args
+   */
+  constructor (fun, args) {
     super();
     this.fun = fun;
-    this.arg = arg;
+    this.args = args;
   }
 
-   eval() {
+  combine(args) {
+    if (args instanceof Ast)
+      args = [args];
+    if (args.length === 0)
+      return this;
+    return new Call(this.fun, [...this.args, ...args]);
+  }
+
+  eval() {
+    // if subtrees changed, return new self
+    let change = 0;
     const f = this.fun.eval();
-    const x = this.arg.eval();
-    return f && x ? new Call(f ?? this.fun, x ?? this.arg) : null;
+    if (f) {
+      change++;
+      // console.log("change in function: "+this.fun + " => " + f);
+    }
+
+    const args = [];
+    for (let x of this.args) {
+      const next = x.eval();
+      args.push(next ?? x);
+      if (next) {
+        change++;
+        // console.log("change in arg: "+x+" => "+next);
+      }
+    }
+
+    if (change)
+      return (f ?? this.fun).combine(args);
+
+    // if nothing has changed, but there's known combinator, reduce it
+    if (this.fun instanceof Special && this.args.length >= this.fun.arity) {
+      const args = [...this.args]; // shallow copy
+      const enough = args.splice(0, this.fun.arity);
+      const result = this.fun.impl(...enough)
+      // console.log("eval: apply "+this.fun+" to "+enough.join(", ")+" => "+result);
+      return result.combine(args);
+    }
+
+    // no change whatsoever
+    return null;
   }
 
-   toString () {
-    return this.fun.toString()+"("+this.arg.toString()+")";
+  toString () {
+    return this.fun.toString() + this.args.map(x => "("+x+")").join("");
   }
 }
 
@@ -36,82 +80,32 @@ class Value extends Ast {
     super();
     this.name = name;
   }
-   toString () {
+  toString () {
     return this.name;
   }
 }
 
-class I extends Ast {
-   apply(x) {
-    return x;
-  }
-   toString () {
-    return "I";
-  }
-}
-
-class K extends Ast {
-   apply(x) {
-    return new K1(x);
-  }
-   toString () {
-    return "K";
+class Special extends Value {
+  constructor (name, arity, impl) {
+    super(name);
+    this.arity = arity;
+    this.impl  = impl;
   }
 }
 
-class K1 extends Ast {
-  constructor (x) {
-    super();
-    this.x = x;
+class Empty extends Ast {
+  combine(args) {
+    return args.shift().combine(args);
   }
-   apply(unused) {
-    return this.x;
-  }
-   toString () {
-    return "K("+this.x.toString()+")";
-  }
-}
-
-class S extends Ast {
-   apply(x) {
-    return new S1(x);
-  }
-   toString () {
-    return "S";
-  }
-}
-
-class S1 extends Ast {
-  constructor (x) {
-    super();
-    this.x = x;
-  }
-   apply (y) {
-    return new S2(this.x, y);
-  }
-   toString () {
-    return "S("+this.x+")";
-  }
-}
-
-
-class S2 extends S1 {
-  constructor (x, y) {
-    super(x);
-    this.y = y;
-  }
-   apply(z) {
-    return new Call(this.x, z).apply(new Call(this.y, z));
-  }
-   toString () {
-    return super.toString() + "(" + this.y + ")";
+  toString () {
+    return "<empty>";
   }
 }
 
 const known = {
-  S: new S(),
-  K: new K(),
-  I: new I(),
+  I: new Special("I", 1, x => x),
+  K: new Special("K", 2, (x, _) => x),
+  S: new Special("S", 3, (x, y, z) => x.combine([z]).combine([y.combine([z])])),
 }
 
 function ident (token) {
@@ -119,24 +113,25 @@ function ident (token) {
 }
 
 function parse (str) {
-  const tokens = [...str.matchAll(/[SKI]|[a-z]+|[()]/g)].map(x => x[0]);
+  const tokens = [...str.matchAll(/[SKI()]|[a-z]+/g)].map(x => x[0]);
 
-  const empty = known.I;
+  const empty = new Empty;
   const stack = [empty];
 
   for( let c of tokens) {
-    // console.log("parse: found "+c+"; stack = ", stack);
+    // console.log("parse: found "+c+"; stack =", stack.join(", "));
 
     if (c === '(') {
       stack.push(empty);
     } else if( c === ')') {
       const x = stack.pop();
       const f = stack.pop();
-      stack.push(f.apply(x));
+      stack.push(f.combine([x]));
     } else {
       const f = stack.pop();
       const x = ident(c);
-      stack.push(f.apply(x));
+      // console.log("combine", f, x)
+      stack.push(f.combine([x]));
     }
   }
 
@@ -147,6 +142,8 @@ function parse (str) {
 }
 
 let expr = parse(process.argv[2]);
+
+// console.log(expr);
 
 while (expr) {
   console.log(""+expr);
